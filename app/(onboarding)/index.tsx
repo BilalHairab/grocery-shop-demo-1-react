adb6aba69570decdd99dbd972570a855ddd69089
Bilal Ibrahim Hairab <bilal.hairab@gmail.com>
2024-10-05 11:53:35 +0400

    Add on boarding image

--- app/(onboarding)/index.tsx
+++ app/(onboarding)/index.tsx
@@ -3,7 +3,7 @@ import RoundedButton from '@/components/RoundedButton';
 import TitleText from '@/components/TitleText';
 import { useThemeColor } from '@/hooks/useThemeColor';
 import React from 'react';
-import { SafeAreaView, View } from 'react-native';
+import { SafeAreaView, View, Image } from 'react-native';
 import { useDispatch } from 'react-redux';
 import userActions from '@/reducers/user/userActions'
 
@@ -15,7 +15,7 @@ export default function OnBoardingScreen() {
     <SafeAreaView style={{ height: '100%', flexDirection: 'column' }}>
       <View style={{backgroundColor: primaryColor}}>
         <View style={{ backgroundColor: secondBackgroundColor, height: '55%', borderBottomLeftRadius: 50, borderBottomRightRadius: 50 }}>
-
+          <Image source={require("@/assets/images/intro-1585235194.jpg")} style={{width: '100%', height: '100%', alignSelf: 'center', borderRadius: 10}}/>
         </View>
         <View style={{ backgroundColor: primaryColor, height: '45%', marginHorizontal: 20, paddingVertical: 20, rowGap: 30, flexDirection: 'column', alignItems: 'center' }}>
           <TitleText text='Welcome to Grocery Shop' />
